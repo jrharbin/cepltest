ef81df12b474e69670a838726c4d549eb1eb4479
James Harbin <james.harbin@gmail.com>
2018-06-27 03:37:43 +0100

    Perspective changes

--- cepltest.lisp
+++ cepltest.lisp
@@ -13,44 +13,53 @@
 
 (defun init (shape)
   (setf *buf-stream*
-	(let ((bi
-		(ecase shape
-		  (cone (nineveh.mesh.data.primitives:cone-gpu-arrays))
-		  (lattice (nineveh.mesh.data.primitives:lattice-gpu-arrays))
-		  (cube (nineveh.mesh.data.primitives:cube-gpu-arrays))
-		  (cylinder (nineveh.mesh.data.primitives:cylinder-gpu-arrays)))))
-	  (make-buffer-stream (car bi)
-			      :index-array (cadr bi)))))
+	(let ((bi (ecase shape
+		    (cone (nineveh.mesh.data.primitives:cone-gpu-arrays))
+		    (lattice (nineveh.mesh.data.primitives:lattice-gpu-arrays))
+		    (cube (nineveh.mesh.data.primitives:cube-gpu-arrays))
+		    (cylinder (nineveh.mesh.data.primitives:cylinder-gpu-arrays)))))
+	  (make-buffer-stream (car bi) :index-array (cadr bi)))))
 
-(defun-g s-vert ((pos :vec3) &uniform (m :mat4))
-  (let* ((vert (* m (v! pos 1)))
-	 (col (+ pos (v! 1.0 0.5 0.5))))
-    (values vert col)))
+(defun-g s-vert ((pos :vec3)
+		 &uniform (mat :mat4)
+		 (offset :vec3)
+		 (perspective :mat4))
+  (let* ((vert (* perspective (+ (* mat (v! pos 1)) (v! offset 1))))
+	 (col (+ pos (v! 0.5 0.5 0.5))))
+    (values vert (:smooth col))))
 
 (defun-g s-frag ((col :vec3))
+  ;;(v! 0 0 1)
   col)
 
 (defpipeline-g pipe-test ()
   (s-vert :vec3) (s-frag :vec3))
 
-(defun draw-with-matrix (m)
-  (clear)
-  (map-g #'pipe-test *buf-stream* :m m)
-  (swap))
+(defun set-res ()
+  (setf (resolution (current-viewport))
+	(surface-resolution (current-surface))))
 
-(draw-with-matrix (rtg-math.matrix4:rotation-z 1.0))
+(defparameter *persp*
+  (rtg-math.projection:perspective 10f0 10f0 0.1 30f0 60f0)) 
+
+(defun draw-with-matrix (&key mat offset)
+  (set-res)
+  (step-host)
+  (clear)
+  (map-g #'pipe-test *buf-stream* :mat mat :offset offset :perspective *persp*)
+  (swap))
 
 (defun now () (/ (get-internal-real-time) 5000.0))
 
-(defmacro make-shape-loop (name)
-  `(nineveh:def-simple-main-loop ,name (:on-start (lambda () (init ',name)))
+(defparameter *shape* 'cube)
+
+(nineveh:def-simple-main-loop testloop (:on-start (lambda () (init *shape*)))
+  (let ((offsetx (sin (/ (get-internal-real-time) 1500.0)))
+	(offsety (cos (/ (get-internal-real-time) 200.0))))
     (draw-with-matrix
+     :mat
      (rtg-math.matrix4:* (rtg-math.matrix4:rotation-z (now))
-			 (rtg-math.matrix4:rotation-x (/ (now) 0.1))))))
+			 (rtg-math.matrix4:rotation-x (/ (now) 0.5)))
+     :offset (v! offsetx offsety -2.5f0))))
 
-(make-shape-loop cube)
-(make-shape-loop cylinder)
-(make-shape-loop lattice)
-(make-shape-loop cone)
-
-(cube :start)
+(testloop :start)
